9fc45d08c52fd5db5964fe898771222dc69c9787
NicolasR64 <nicolasravaux64@gmail.com>
2022-11-30 15:00:07 +0100

    add dismiss action

--- lib/screens/views/ListCircuitView.dart
+++ lib/screens/views/ListCircuitView.dart
@@ -1,10 +1,42 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+const List<Map<String, dynamic>> listMapping = [
+  {
+    'name': "#first",
+  },
+  {
+    'name': "#second",
+  },
+];
 
 class ListCircuitView extends StatelessWidget{
   const ListCircuitView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text("CircuitView"));
+    return ListView.builder(
+        itemCount: listMapping.length,
+        itemBuilder: (context, index) {
+          return Dismissible(
+            key: Key(index.toString()),
+            onDismissed: (direction) {
+              ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text('$index dismissed'),
+                  ),
+              );
+            },
+            background: Container(
+              color: Colors.red,
+            ),
+            child:Card(
+              child :ListTile(
+                title: Text(listMapping[index]['name']),
+              ),
+            ),
+          );
+        }
+    );
   }
 }
