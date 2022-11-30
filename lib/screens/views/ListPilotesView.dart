999633cb02129925c29fb53d3e812c2bcf177eb1
NicolasR64 <nicolasravaux64@gmail.com>
2022-11-30 14:34:46 +0100

    add view

--- lib/screens/views/ListPilotesView.dart
+++ lib/screens/views/ListPilotesView.dart
@@ -5,6 +5,6 @@ class ListPilotesView extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text("list hashtags"));
+    return const Center(child: Text("ListPilotesView"));
   }
 }
