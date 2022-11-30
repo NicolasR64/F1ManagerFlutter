c0d4b3860988b39df6cbb58c559bef320a0ca27b
NicolasR64 <nicolasravaux64@gmail.com>
2022-11-30 14:34:17 +0100

    add view

---
+++ lib/screens/views/ListPilotesView.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class ListPilotesView extends StatelessWidget{
+  const ListPilotesView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text("list hashtags"));
+  }
+}
