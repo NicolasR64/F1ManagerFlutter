e499259bf6c3bb26f69af2814f6bc0621180f04a
Lorys <lorys.massaux@hotmail.com>
2022-12-07 11:18:33 +0100

    Update Pilote - Add Map and toString

--- lib/screens/models/Pilote.dart
+++ lib/screens/models/Pilote.dart
@@ -1,8 +1,11 @@
+import 'Ecurie.dart';
+
 class Pilote{
   final int id;
   final String nom;
   final String prenom;
   final String numero;
+  final Ecurie ecurie;
   final int points;
 
   const Pilote({
@@ -10,6 +13,23 @@ class Pilote{
     required this.nom,
     required this.prenom,
     required this.numero,
+    required this.ecurie,
     required this.points,
 });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'nom': nom,
+      'prenom': prenom,
+      'numero': numero,
+      'ecurie': ecurie,
+      'points': points,
+    };
+  }
+
+  @override
+  String toString() {
+    return 'Pilote{id: $id, nom: $nom, prenom: $prenom, numero: $numero, ecurie: $ecurie, points: $points}';
+  }
 }
