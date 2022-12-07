3b6a84d880f627262d9fd86ad0b2b8f84a3c80d0
Lorys <lorys.massaux@hotmail.com>
2022-12-07 11:08:14 +0100

    ADD Pilote modèle + Modification config pubspec.yaml

---
+++ lib/screens/models/Pilote.dart
@@ -0,0 +1,16 @@
+class Pilote{
+  final int id;
+  final String nom;
+  final String prenom;
+  final String numero;
+  final int points;
+
+  const Pilote({
+    required this.id,
+    required this.nom,
+    required this.prenom,
+    required this.numero,
+    required this.points,
+});
+
+}
